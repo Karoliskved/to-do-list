669d08605cf19b2eea56ff6eca4e8c8472f88634
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-25 13:06:38 +0300

    add status atribute to task object

--- src/task.js
+++ src/task.js
@@ -1,9 +1,14 @@
 
 const Task = (title, description, dueDate, priority) => {
+    let status=false;
     const getTitle = () => title;
     const getDescription = () => description;
     const getDueDate = () => dueDate;
     const getPriority = () => priority;
-    return { getTitle, getDescription, getDueDate, getPriority }
+    const getStatus = () => status;
+    const changeStatus=()=>{
+        status=!status
+    }
+    return { getTitle, getDescription, getDueDate, getPriority, getStatus, changeStatus }
 }
 export default Task
