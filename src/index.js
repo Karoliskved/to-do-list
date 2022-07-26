bfba164d25faa431af245d8fd6f4b67922d6132d
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-26 12:00:38 +0300

    clear form on submit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import Task from './task.js'
 import Project from './project.js'
 import createCard from './taskCard.js'
 
+const container = document.querySelector('#container');
+const formContainer = document.querySelector('.formContainer');
+const submitButton = document.querySelector('.submitButton');
+const inputTitle = document.querySelector('#inputTitle');
+const inputDesc = document.querySelector('#inputDesc');
+const inputDate = document.querySelector('#inputDate');
+const addButton = document.querySelector('.formButton')
+const taskForm = document.querySelector('#newTask')
 console.log('test')
 const task1 = Task('get food', 'get food for dog', '2022-05-05', 'urgent')
 const task2 = Task('get stuff', 'get stuff for me', '2022-05-06', 'non- urgent')
@@ -18,67 +26,18 @@ console.log(Project1.getTasks()[0].getStatus())
 console.log(Project1.getTasks()[1].getStatus())
 document.querySelector('#container').appendChild(createCard(Project1.getTasks()[0]))
 document.querySelector('#container').appendChild(createCard(Project1.getTasks()[1]))
-const addButton = document.createElement('button')
-addButton.classList.add('submitButton')
-addButton.textContent = 'add task'
+
 addButton.addEventListener('click', () => {
-    const inputDiv = document.createElement('div');
-    const inputTitle = document.createElement('input');
-    inputTitle.setAttribute('id', 'inputTitle');
-
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'inputTitle');
-    titleLabel.textContent='title';
-
-    const inputDesc = document.createElement('input');
-    inputDesc.setAttribute('id', 'inputDesc');
-
-    const descLabel = document.createElement('label');
-    descLabel.setAttribute('for', 'inputDesc');
-    descLabel.textContent='description';
-
-    const inputDate = document.createElement('input');
-    inputDate.setAttribute('id', 'inputDate');
-    inputDate.type='date';
-
-    const dateLabel = document.createElement('label');
-    dateLabel.setAttribute('for', 'inputDate');
-    dateLabel.textContent='Due date';
-
-    const nonUrgentLabel = document.createElement('label');
-    nonUrgentLabel.setAttribute('for', 'urgency');
-    nonUrgentLabel.textContent='non-urgent';
-    const nonUrgent = document.createElement('input');
-    nonUrgent.setAttribute('id', 'urgency');
-    nonUrgent.type='radio';
-    const midUrgentLabel = document.createElement('label');
-    midUrgentLabel.setAttribute('for', 'urgency');
-    midUrgentLabel.textContent='mildly urgent';
-    const midUrgent = document.createElement('input');
-    midUrgent.setAttribute('id', 'urgency');
-    midUrgent.type='radio';
-    const highUrgentLabel = document.createElement('label');
-    highUrgentLabel.setAttribute('for', 'urgency');
-    highUrgentLabel.textContent='highly urgent';
-    const highUrgent = document.createElement('input');
-    highUrgent.setAttribute('id', 'urgency');
-    highUrgent.type='radio';
-    
-
-
-    inputDiv.appendChild(titleLabel);
-    inputDiv.appendChild(inputTitle);
-    inputDiv.appendChild(descLabel);
-    inputDiv.appendChild(inputDesc);
-    inputDiv.appendChild(dateLabel);
-    inputDiv.appendChild(inputDate);
-    inputDiv.appendChild(nonUrgent);
-    inputDiv.appendChild(nonUrgentLabel);
-    inputDiv.appendChild(midUrgent);
-    inputDiv.appendChild(midUrgentLabel);
-    inputDiv.appendChild(highUrgent);
-    inputDiv.appendChild(highUrgentLabel);
-    document.querySelector('#container').appendChild(inputDiv);
+    formContainer.hidden = false
+})
+submitButton.addEventListener('click', () => {
+    formContainer.hidden = true
+    const newTask = Task(inputTitle.value, inputDesc.value, inputDate.value, taskForm.elements['urgency'].value)
+    inputTitle.value = "";
+    inputDesc.value = "";
+    inputDate.value = "";
+    document.querySelectorAll('#urgency').forEach (element => element.checked=false);
+    container.appendChild(createCard(newTask))
 
 })
-document.querySelector('#container').appendChild(addButton)
+
